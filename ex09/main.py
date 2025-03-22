5aab7cee89c3831164b959131fdb89b13e00d8c4
Diogo Santana Malosti <dsmalosti@gmail.com>
2025-03-22 11:08:39 -0300

    Estudo de herança

---
+++ ex09/main.py
@@ -0,0 +1,17 @@
+from classes import *
+
+c1 = Cliente('Diogo', 20)
+print(c1.nome)
+c1.comprar()
+c1.falar()
+print()
+
+p1 = Pessoa('Luiza', 40)
+print(p1.nome)
+p1.falar()
+print()
+
+a1 = Aluno('Felipe', 15)
+print(a1.nome)
+a1.falar()
+a1.estudar()
